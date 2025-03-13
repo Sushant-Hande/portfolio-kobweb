79e5bc40c8d68afeb45707ba8a2070aa73d002f5
Sushant Hande <sushanthande1@gmail.com>
2025-03-13 12:55:31 +0530

    Smooth scrolling issue is fixed

--- site/src/jsMain/kotlin/com/sushanthande/portfolio/AppEntry.kt
+++ site/src/jsMain/kotlin/com/sushanthande/portfolio/AppEntry.kt
@@ -1,11 +1,11 @@
 package com.sushanthande.portfolio
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import com.sushanthande.portfolio.utils.Res
 import com.varabyte.kobweb.compose.css.ScrollBehavior
-import com.varabyte.kobweb.compose.ui.graphics.Color
-import com.varabyte.kobweb.compose.ui.graphics.Colors
-import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.modifiers.minHeight
+import com.varabyte.kobweb.compose.ui.modifiers.scrollBehavior
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
@@ -15,11 +15,16 @@ import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
 import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.palette.background
 import com.varabyte.kobweb.silk.theme.colors.palette.color
-import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.vh
 
 @App
 @Composable
 fun AppEntry(content: @Composable () -> Unit) {
+    // Apply smooth scrolling to the document
+    LaunchedEffect(Unit) {
+        js("document.documentElement.style.scrollBehavior = 'smooth'") as Unit
+    }
+
     SilkApp {
         Surface(SmoothColorStyle.toModifier().minHeight(100.vh).scrollBehavior(ScrollBehavior.Smooth)) {
             content()
